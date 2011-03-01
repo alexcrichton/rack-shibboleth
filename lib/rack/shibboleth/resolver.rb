9bdf971701cb8c87d9b22ca27da0b82d938da6a4
Alex Crichton <alex@alexcrichton.com>
2011-03-01 12:58:11 -0500

    Document the return value of Shibboleth::Resolver#from_response

--- lib/rack/shibboleth/resolver.rb
+++ lib/rack/shibboleth/resolver.rb
@@ -6,10 +6,14 @@ module Rack
 
       # Creates a new Resolver from the IdP's response, using the given private
       # key to decrypt the response.
-      # 
+      #
       # @param [String] resp the raw response from the IdP
       # @param [OpenSSL::PKey::RSA] private_key the private key which will be
       #        used to decrypt the response
+      #
+      # @return [Rack::Shibboleth::Resolver, false] either the resolver object
+      #         for the specified response or false if the response could not
+      #         be decode and/or verified
       def self.from_response resp, private_key
         xml = Rack::Utils.parse_query(resp)
         xml = Base64.decode64 xml['SAMLResponse']
@@ -22,7 +26,7 @@ module Rack
 
       # Creates a new resolver for the specified assertion document which was
       # decoded from a response to the IdP
-      # 
+      #
       # @param [Nokogiri::XML::Document] assertion the parsed version of the
       #        xml received from the IdP
       def initialize assertion
@@ -30,21 +34,21 @@ module Rack
       end
 
       # The exact time that the response was issued at
-      # 
+      #
       # @return [Time] the time this response was issued at
       def issued
         Time.parse @doc.xpath('//saml2:Assertion').first['IssueInstant']
       end
 
       # The entity name of the IdP who issued the response
-      # 
+      #
       # @return [String] the entity name of the Idp
       def issuer
         @doc.xpath('//saml2:Issuer').text
       end
 
       # Extracts the conditions under which this response is valid.
-      # 
+      #
       # @return [Hash] with the following keys:
       #           :after  - [Time] the response is only valid after this time
       #           :before - [Time] the response is only valid before this time
